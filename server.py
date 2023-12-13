8a9d8334cc99ffb43270407556d7b01a71656094
wishba <wisnubayu34@gmail.com>
2023-12-13 14:46:08 +0700

    update and delete

--- server.py
+++ server.py
@@ -76,6 +76,82 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(json.dumps({"message": "Task added successfully"}).encode())
 
+    def do_PUT(self):
+        # Extract path information
+        path = self.path.split("/")
+
+        # Check if a specific task is targeted for update
+        if len(path) >= 3 and path[2].isdigit():
+            task_id = int(path[2])
+
+            # Get request body data
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length).decode()
+            data = json.loads(body)
+
+            # Extract updated task name
+            updated_task_name = data.get('task_name')
+
+            # Connect to the database
+            try:
+                connection = psycopg2.connect(host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)
+            except Exception as e:
+                print(f"Error connecting to database: {e}")
+                self.send_response(500)
+                self.end_headers()
+                return
+
+            # Get cursor object
+            cursor = connection.cursor()
+
+            # Update the task with the provided ID
+            cursor.execute("UPDATE tasks SET task_name = %s WHERE id = %s", (updated_task_name, task_id))
+            connection.commit()
+
+            # Close cursor and connection
+            cursor.close()
+            connection.close()
+
+            # Send a success response
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({"message": "Task updated successfully"}).encode())
+
+    def do_DELETE(self):
+        # Extract path information
+        path = self.path.split("/")
+
+        # Check if a specific task is targeted for deletion
+        if len(path) >= 3 and path[2].isdigit():
+            task_id = int(path[2])
+
+            # Connect to the database
+            try:
+                connection = psycopg2.connect(host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)
+            except Exception as e:
+                print(f"Error connecting to database: {e}")
+                self.send_response(500)
+                self.end_headers()
+                return
+
+            # Get cursor object
+            cursor = connection.cursor()
+
+            # Delete the task with the provided ID
+            cursor.execute("DELETE FROM tasks WHERE id = %s", (task_id,))
+            connection.commit()
+
+            # Close cursor and connection
+            cursor.close()
+            connection.close()
+
+            # Send a success response
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({"message": "Task deleted successfully"}).encode())
+
 PORT = 8000
 
 with HTTPServer(('', PORT), MyRequestHandler) as server:
