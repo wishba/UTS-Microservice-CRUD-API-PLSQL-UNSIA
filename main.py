65c647c0248e7b3d6bea597069724399afea3bae
wishba <wisnubayu34@gmail.com>
2023-12-13 22:14:35 +0700

    rename to main

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 import psycopg2
+import os
+from dotenv import load_dotenv, dotenv_values
 
-DB_HOST = "localhost"
-DB_NAME = "todo"
-DB_USER = "postgres"
-DB_PASSWORD = "password"
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 class MyRequestHandler(BaseHTTPRequestHandler):
 
