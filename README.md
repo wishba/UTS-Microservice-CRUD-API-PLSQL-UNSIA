a2d25a9b7c52e875029ceef695a9b87439db1275
wishba <wisnubayu34@gmail.com>
2023-12-13 10:15:20 +0700

    crud

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # UTS Microservice CRUD API PLSQL UNSIA
+
+how to run:
+
+1. ./venv/Scripts/activate
+2. cd ./app/
+3. uvicorn main:app --reload
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+import models
+from routes import router
+from config import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-async def HOME():
-  return "Selamat Datang"
+app.include_router(router, prefix="/book", tags=["book"])
